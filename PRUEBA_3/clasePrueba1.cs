a3baf46f48635035c801edc488d84ec6e78ee1d8
ALEXANDER DIAZ <ALEXANDER DIAZ@ALEXANDERDC-24>
2023-05-12 19:01:52 -0500

    Agregar archivos de proyecto.

---
+++ PRUEBA_3/clasePrueba1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRUEBA_3
+{
+    internal class clasePrueba1
+
+    {
+        String a 
+
+    }
+}
